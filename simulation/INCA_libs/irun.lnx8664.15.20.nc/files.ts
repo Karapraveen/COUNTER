e223a9a4dae91b2ff3fd7c78fa184ef7b572b327
Karapraveen <143269506+Karapraveen@users.noreply.github.com>
2024-01-20 01:37:46 +0530

    Add files via upload

---
+++ simulation/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1693990904 /home/vlsi6/Desktop/kumar_dicd/counter/simulation/cds.lib
+1693990910 /home/vlsi6/Desktop/kumar_dicd/counter/simulation/hdl.var
+1693990209 /home/vlsi6/Desktop/kumar_dicd/counter/simulation/counter.v
+1693990856 /home/vlsi6/Desktop/kumar_dicd/counter/simulation/counter_tb.v
